0ecbb5d7187c421178e4f75d1b09e8ec59fc0693
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-11-28 14:54:38 +0200

    Cmake

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello, world!";
+  return 0;
+}
+
+// #include <SDL.h>
+
+// int main(int argc, char *argv[]) {
+//   SDL_Init(SDL_INIT_VIDEO);
+//   SDL_Delay(1000);
+//   SDL_Quit();
+//   return 0;
+// }
