901986b062c94a528db816d8cc6734b005e7720c
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-11-28 20:20:17 +0200

    Windows config

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,3 @@ int main(int argc, char *argv[]) {
   std::cout << "Hello, world!" << std::endl;
   return 0;
 }
-
-// #include <SDL.h>
-
-// int main(int argc, char *argv[]) {
-//   SDL_Init(SDL_INIT_VIDEO);
-//   SDL_Delay(1000);
-//   SDL_Quit();
-//   return 0;
-// }
