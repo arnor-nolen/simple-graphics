340a74f526bdd55fb409f0f6695390ffa8b83462
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-12-03 10:59:53 +0200

    load_shaders

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,93 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
+
+GLuint load_shaders(std::string vert_path, std::string frag_path) {
+  std::stringstream vert_shader_source, frag_shader_source;
+  std::ifstream vert_shader(vert_path);
+  if (!vert_shader) {
+    std::cout << "Can't load vertex shader file!" << std::endl;
+    return 0;
+  }
+  std::ifstream frag_shader(frag_path);
+  if (!frag_shader) {
+    std::cout << "Can't load fragment shader file!" << std::endl;
+    return 0;
+  }
+
+  vert_shader_source << vert_shader.rdbuf();
+  frag_shader_source << frag_shader.rdbuf();
+  std::string vert_shader_str = vert_shader_source.str();
+  std::string frag_shader_str = frag_shader_source.str();
+  const char *vert_shader_ptr = vert_shader_str.c_str();
+  const char *frag_shader_ptr = frag_shader_str.c_str();
+
+  // Create and compile the vertex shader
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vert_shader_ptr, NULL);
+  glCompileShader(vertexShader);
+
+  GLint result = GL_FALSE;
+  int info_log_length = 0;
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &info_log_length);
+  if (info_log_length > 0) {
+    std::vector<char> error_message(info_log_length + 1);
+    glGetShaderInfoLog(vertexShader, info_log_length, NULL, &error_message[0]);
+    std::cout << "Error compiling vertex shader!" << std::endl;
+    for (auto &i : error_message)
+      std::cout << i;
+    std::cout << std::endl;
+  }
+
+  // Create and compile the fragment shader
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &frag_shader_ptr, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &info_log_length);
+  if (info_log_length > 0) {
+    std::vector<char> error_message(info_log_length + 1);
+    glGetShaderInfoLog(fragmentShader, info_log_length, NULL,
+                       &error_message[0]);
+    std::cout << "Error compiling fragment shader!" << std::endl;
+    for (auto &i : error_message)
+      std::cout << i;
+    std::cout << std::endl;
+  }
+
+  // Link the vertex and fragment shader into a shader program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  // Check the program
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+  glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &info_log_length);
+  if (info_log_length > 0) {
+    std::vector<char> error_message(info_log_length + 1);
+    glGetProgramInfoLog(shaderProgram, info_log_length, NULL,
+                        &error_message[0]);
+    std::cout << "Error compiling shader program!" << std::endl;
+    for (auto &i : error_message)
+      std::cout << i;
+    std::cout << std::endl;
+  }
+
+  glDetachShader(shaderProgram, vertexShader);
+  glDetachShader(shaderProgram, fragmentShader);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  return shaderProgram;
+}
 
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
@@ -52,33 +139,8 @@ int main(int argc, char *argv[]) {
                GL_STATIC_DRAW);
 
   // Reading shaders from files
-  std::stringstream vert_shader_source, frag_shader_source;
-  std::ifstream vert_shader("./src/shaders/shader.vert");
-  std::ifstream frag_shader("./src/shaders/shader.frag");
-  vert_shader_source << vert_shader.rdbuf();
-  frag_shader_source << frag_shader.rdbuf();
-  std::string vert_shader_str = vert_shader_source.str();
-  std::string frag_shader_str = frag_shader_source.str();
-  const char *vert_shader_ptr = vert_shader_str.c_str();
-  const char *frag_shader_ptr = frag_shader_str.c_str();
-
-  // Create and compile the vertex shader
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vert_shader_ptr, NULL);
-  glCompileShader(vertexShader);
-
-  // Create and compile the fragment shader
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &frag_shader_ptr, NULL);
-  glCompileShader(fragmentShader);
-
-  // Link the vertex and fragment shader into a shader program
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glBindFragDataLocation(shaderProgram, 0, "outColor");
-  glLinkProgram(shaderProgram);
-  glUseProgram(shaderProgram);
+  GLint shaderProgram =
+      load_shaders("./src/shaders/shader1.vert", "./src/shaders/shader2.frag");
 
   // Specify the layout of the vertex data
   GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
@@ -106,11 +168,7 @@ int main(int argc, char *argv[]) {
     SDL_GL_SwapWindow(window);
   }
   glDeleteProgram(shaderProgram);
-  glDeleteShader(fragmentShader);
-  glDeleteShader(vertexShader);
-
   glDeleteBuffers(1, &vbo);
-
   glDeleteVertexArrays(1, &vao);
 
   SDL_GL_DeleteContext(context);
