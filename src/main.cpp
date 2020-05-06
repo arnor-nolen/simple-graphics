d14aab945f52bf9a1d6e51f6706fe93ff677f1a1
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2020-05-06 21:24:07 +0300

    Added Conan support

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,4 @@
-#include <GL/glew.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_opengl.h>
-#include <algorithm>
-#include <chrono>
-#include <fstream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
+#include "pch.h"
 
 GLuint texture_id;
 
---
+++ src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_opengl.h>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
